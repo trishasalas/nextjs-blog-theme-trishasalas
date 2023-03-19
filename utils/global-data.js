f417a651f6a6dfca6b81e5f143fe83eaf39b36c2
Trisha Salas <trisha@trishasalas.com>
2023-03-19 13:47:00 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Trisha Salas';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Digital Accessibility Consultatn';
+    : 'Accessibility Consultant';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
